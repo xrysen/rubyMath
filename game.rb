a5a574851b111eccbdfd79743057e451e5fc3ed3
Sean Oyler <xrysen@gmail.com>
2021-01-04 19:02:41 +0000

    Adds logic to handle end of game

--- game.rb
+++ game.rb
@@ -21,6 +21,26 @@ class Game
     else
       @current_turn = @p1
     end
+    if @state != "Game Over"
+      puts "----- NEW TURN -----"
+    end
+  end
+
+  def check_score
+    if @p1.lives == 0 || @p2.lives == 0
+      @state = "Game Over"
+    end
+  end
+
+  def game_over
+    if @p1.lives > @p2.lives
+      puts "#{@p1.name} wins with a score of #{@p1.lives}/3"
+    else
+      puts "#{@p2.name} wins with a score of #{@p2.lives}/3"
+    end
+
+    puts "----- GAME OVER -----"
+    puts "Goodbye!"
   end
 
   def game_loop
@@ -29,14 +49,17 @@ class Game
       puts "Should be #{@answer}"
       answer = gets.chomp
       if answer.to_i == @answer
-        puts "Good job!"
+        puts "#{@current_turn.name}: YES! You are correct!"
+        puts "P1: #{@p1.lives}/3 vs. P2: #{@p2.lives}/3"
         change_turn()
       else
         puts "#{@current_turn.name}: Seriously? No!"
         @current_turn.take_life()
         puts "P1: #{@p1.lives}/3 vs. P2: #{@p2.lives}/3"
+        check_score()
         change_turn()
       end
     end 
+    game_over()
   end
 end
