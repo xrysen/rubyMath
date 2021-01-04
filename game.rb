641e051aecc5572d98ccdd61939af378e6b2282e
Sean Oyler <xrysen@gmail.com>
2021-01-04 18:32:35 +0000

    Changes turn after each question

--- game.rb
+++ game.rb
@@ -3,10 +3,33 @@ require "./player"
 class Game
   attr_accessor :current_turn, :state
 
+  def initialize(p1, p2)
+    @state = "Playing"
+    @p1 = p1
+    @p2 = p2
+    @current_turn = @p1
+  end
+
+  def generate_question
+    @num1 = rand(1..20)
+    @num2 = rand(1..20)
+    @answer = @num1 + @num2
+    puts "#{@current_turn.name} What's #{@num1} plus #{@num2}?"
+  end
+
   def game_loop
     while @state != "Game Over"
-      @state = gets.chomp
-      puts "#{state}"
+      generate_question()
+      puts "Should be #{@answer}"
+      answer = gets.chomp
+      if answer.to_i == @answer
+        puts "Good job!"
+        if @current_turn == @p1 
+          @current_turn = @p2
+        else
+          @current_turn = @p1
+        end
+      end
     end 
   end
 end
--- main.rb
+++ main.rb
@@ -4,5 +4,5 @@ require "./player"
 p1 = Player.new("Player 1")
 p2 = Player.new("Player 2")
 
-game = Game.new()
+game = Game.new(p1, p2)
 game.game_loop()
